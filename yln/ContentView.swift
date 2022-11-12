91faccc5a2c7b57ed8bb151eb56b7796e50c8188
leejunghoo515@naver.com <leejunghoo515@naver.com>
2022-11-12 23:16:54 +0900

    feat:Button testt

--- yln/ContentView.swift
+++ yln/ContentView.swift
@@ -6,17 +6,26 @@
 //
 
 import SwiftUI
-import CoreData
+//import CoreData
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Swift!")
-        Text("Hello, SwiftUI!")
-    }
-}
+        Button {
+            print("Hit")
+        } label: {
+            Text("Leo")
+                .padding()
+                .frame(width: 150)
+                .foregroundColor(.white)
+                .background(.blue)
+                .cornerRadius(13)
+        }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        }
+    }
+
