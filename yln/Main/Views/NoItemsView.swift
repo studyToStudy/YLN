c43de35627e53e4ce873cc7e05cc284e05f5d6b5
leejunghoo515@naver.com <leejunghoo515@naver.com>
2022-11-19 22:56:24 +0900

    Animation 추가

--- yln/Main/Views/NoItemsView.swift
+++ yln/Main/Views/NoItemsView.swift
@@ -8,16 +8,46 @@
 import SwiftUI
 
 struct NoItemsView: View {
+    
+    @State var animate: Bool = false
+    
     var body: some View {
         ScrollView {
             VStack {
                 Text("There are no Items!")
                     .font(.title)
                     .fontWeight(.semibold)
+                NavigationLink(destination: AddView(), label: {
+                    Text("Add TODO")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(animate ? Color.red : Color.accentColor)
+                        .cornerRadius(10)
+                })
+                .padding()
+//                .scaleEffect(animate ? 1.1 : 1.0)
+//                .offset(y: animate ? -7 : 0)
             }
             .multilineTextAlignment(.center)
+            .padding(40)
+            .onAppear(perform: addAnimation)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    
+    func addAnimation() {
+        guard !animate else { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+            ) {
+                animate.toggle()
+            }
+        }
     }
 }
 
